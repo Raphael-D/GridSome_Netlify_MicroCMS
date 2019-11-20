9539d32356b7182b92a4808164f522f7165df7e2
Raphael-D <31120433+Raphael-D@users.noreply.github.com>
2019-11-20 15:31:57 +0900

    modified:   gridsome.server.js debug code delete.

--- gridsome.server.js
+++ gridsome.server.js
@@ -6,34 +6,19 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 const axios = require('axios')
 module.exports = function (api) {
-  function fetchArticles() {
-      axios
-        .get('https://codehack.microcms.io/api/v1/blog', {
-            headers: {'X-API-KEY': '6ccedd0a-a90d-45f3-801f-1a10abf108f4'}
-        })
-        .then(res => {
-            // this.blog = res.data.contents;
-            // return this.blog
-            return res.data.contents;
-        })
-        .catch(err => {
-            console.log(err);
-        })
-  }
-  
-  
 
+  // Get microCMS data
   api.createPages(async ({ createPage }) => {
     let mydata = await axios.get('https://codehack.microcms.io/api/v1/blog', {
       headers: {'X-API-KEY': '6ccedd0a-a90d-45f3-801f-1a10abf108f4'}
     }).then(res => {
-      console.log('successsssssssssssssssssssssssssssss')
       console.log(res.data.contents)
       return res.data.contents;
     }).catch(err => {
-      console.log('hoooooooooooooooooooooooooogeeeeeeeeeeeeeeeee')
       console.log(err);
     })
+
+    // Enable iterator to mydata
     mydata[Symbol.iterator] = function () {
       let index = 0;
       return {
@@ -46,9 +31,9 @@ module.exports = function (api) {
         }
       };
     };
-    // console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!-------->>>>>>>>>>>>>>>>>>>FETCH ARTICLES -> ', mydata)
+
+    // Take out permalink, then push to keys.
     for(const items of mydata) {
-      // console.log('MY DATA ITEMS ===?/?', items)
       createPage(({
         path: '/works/' + items.permalink,
         component: './src/pages/Works.vue'
@@ -58,34 +43,4 @@ module.exports = function (api) {
   api.loadSource(({ addCollection }) => {
     // Use the Data Store API here: https://gridsome.org/docs/data-store-api/
   })
-  // let blogs = fetchArticles();
-  // for(let items in blogs) {
-  //   api.createPages(({ createPage }) => {
-  //     createPage({
-  //       path: '/works/' + blogs[items].permalink,
-  //       component: './src/pages/Works.vue'
-  //     })
-  //   })
-  // }
-  const hoge01 = 'hoge01';
-  // api.createPages(({ createPage }) => {
-  //   createPage({
-  //     path: '/works/' + hoge01,
-  //     component: './src/pages/Works.vue'
-  //   })
-  //   let blogs = fetchArticles()
-  //   for(let items in blogs) {
-  //     createPage({
-  //       path: '/works/' + blogs[items].permalink,
-  //       component: './src/pages/Works.vue'
-  //     })
-  //   }
-  // })
-  // api.createManagedPages(({ createPage }) => {
-  //   createPage({
-  //     path: '/my-page',
-  //     component: './src/templates/MyPage.vue'
-  //   })
-  // })
-
 }
