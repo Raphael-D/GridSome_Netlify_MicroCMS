9fa2ecb064065a3091cac3854da0060a5ae4ccf1
Raphael-D <31120433+Raphael-D@users.noreply.github.com>
2019-11-20 16:04:30 +0900

    Change of site title

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,7 +5,7 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome hogehoge',
+  siteName: 'No ql',
   // siteUrl: '/',
   plugins: [],
   // templates: {
