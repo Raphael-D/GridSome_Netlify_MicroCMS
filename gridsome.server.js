ddc35261f36568e548be2645e856c7561b2e6b18
Raphael-D <31120433+Raphael-D@users.noreply.github.com>
2019-11-20 11:04:19 +0900

    Changes to be committed: 	modified:   gridsome.server.js 	deleted:    src/pages/._Works.vue 	modified:   src/pages/Works.vue
    Added to Dynamic routing function for single page of works-post.

--- gridsome.server.js
+++ gridsome.server.js
@@ -4,13 +4,88 @@
 
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
-
+const axios = require('axios')
 module.exports = function (api) {
+  function fetchArticles() {
+      axios
+        .get('https://codehack.microcms.io/api/v1/blog', {
+            headers: {'X-API-KEY': '6ccedd0a-a90d-45f3-801f-1a10abf108f4'}
+        })
+        .then(res => {
+            // this.blog = res.data.contents;
+            // return this.blog
+            return res.data.contents;
+        })
+        .catch(err => {
+            console.log(err);
+        })
+  }
+  
+  
+
+  api.createPages(async ({ createPage }) => {
+    let mydata = await axios.get('https://codehack.microcms.io/api/v1/blog', {
+      headers: {'X-API-KEY': '6ccedd0a-a90d-45f3-801f-1a10abf108f4'}
+    }).then(res => {
+      console.log('successsssssssssssssssssssssssssssss')
+      console.log(res.data.contents)
+      return res.data.contents;
+    }).catch(err => {
+      console.log('hoooooooooooooooooooooooooogeeeeeeeeeeeeeeeee')
+      console.log(err);
+    })
+    mydata[Symbol.iterator] = function () {
+      let index = 0;
+      return {
+        next() {
+          if (mydata.length <= index) {
+            return {done: true};
+          } else {
+            return {value: mydata[index++]};
+          }
+        }
+      };
+    };
+    // console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!-------->>>>>>>>>>>>>>>>>>>FETCH ARTICLES -> ', mydata)
+    for(const items of mydata) {
+      // console.log('MY DATA ITEMS ===?/?', items)
+      createPage(({
+        path: '/works/' + items.permalink,
+        component: './src/pages/Works.vue'
+      }))
+    }
+  })
   api.loadSource(({ addCollection }) => {
     // Use the Data Store API here: https://gridsome.org/docs/data-store-api/
   })
+  // let blogs = fetchArticles();
+  // for(let items in blogs) {
+  //   api.createPages(({ createPage }) => {
+  //     createPage({
+  //       path: '/works/' + blogs[items].permalink,
+  //       component: './src/pages/Works.vue'
+  //     })
+  //   })
+  // }
+  const hoge01 = 'hoge01';
+  // api.createPages(({ createPage }) => {
+  //   createPage({
+  //     path: '/works/' + hoge01,
+  //     component: './src/pages/Works.vue'
+  //   })
+  //   let blogs = fetchArticles()
+  //   for(let items in blogs) {
+  //     createPage({
+  //       path: '/works/' + blogs[items].permalink,
+  //       component: './src/pages/Works.vue'
+  //     })
+  //   }
+  // })
+  // api.createManagedPages(({ createPage }) => {
+  //   createPage({
+  //     path: '/my-page',
+  //     component: './src/templates/MyPage.vue'
+  //   })
+  // })
 
-  api.createPages(({ createPage }) => {
-    // Use the Pages API here: https://gridsome.org/docs/pages-api/
-  })
 }
--- src/pages/Works.vue
+++ src/pages/Works.vue
@@ -1,6 +1,6 @@
 <template>
   <Layout>
-
+    <div class="l-wrapper">
     <!-- Learn how to use images here: https://gridsome.org/docs/images -->
     <g-image alt="Example image" src="~/favicon.png" width="135" />
 
@@ -15,10 +15,51 @@
       <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
       <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
     </p>
-
-    <ul class="blog-list">
-      {{ blog.permalink }}
+    <br />
+    <br />
+    <table class="blog-list">
+      <tr>
+        <th>id</th>
+        <th>createdAt</th>
+        <th>updatedAt</th>
+        <th>permalink</th>
+        <th>page_title</th>
+        <th>hero_image</th>
+        <th>content</th>
+      </tr>
+      <tr v-for="item in blog" :key="item.id">
+        <td>{{ item.id }}</td>
+        <td>{{ item.createdAt }}</td>
+        <td>{{ item.updatedAt }}</td>
+        <td>{{ item.permalink }}</td>
+        <td>{{ item.page_title }}</td>
+        <td>{{ item.hero_image }}</td>
+        <td>{{ item.content }}</td>
+      </tr>
+    </table>
+    <br />
+    <br />
+    <article v-if="singlePostView">
+      <h2>個別ページ</h2>
+      ID：{{ singlePost.id }}<br />
+      投稿日：{{ singlePost.createdAt }}<br />
+      更新日：{{ singlePost.updatedAt }}<br />
+      パーマリンク：{{ singlePost.permalink }}<br />
+      タイトル：{{ singlePost.page_title }}<br />
+      画像：{{ singlePost.hero_image }}<br />
+      コンテンツ：<div v-html="singlePost.content"></div><br />
+    </article>
+    <ul>
+      <li v-for="item in blog" :key="item.id">
+      <button @click="movePost(item.permalink)">
+        {{ item.page_title }}:{{ item.permalink }}
+      </button>
+      </li>
     </ul>
+    <nav class="local-link">
+        <a @click="movePost(item.permalink)" v-for="item in blog" :key="item.id" class="local-link__item" :href="'/works/' + item.permalink">{{ item.page_title }}</a>
+      </nav>
+    </div>
   </Layout>
 </template>
 
@@ -30,9 +71,9 @@ export default {
   },
   data() {
     return {
-      blog: {
-          permalink: null
-      }
+      blog: null,
+      singlePost: [],
+      getPost: null
     }
   },
   methods: {
@@ -42,35 +83,82 @@ export default {
                 headers: {'X-API-KEY': '6ccedd0a-a90d-45f3-801f-1a10abf108f4'}
             })
             .then(res => {
-                // console.log(res.data);
-                // this.blog = res.data.contents
-                for(const content of res.data.contents) {
-                    this.blog = content;
-                }
-
+                this.blog = res.data.contents;
+                console.log(this.blog[0].permalink, 'fjdajfka')
+                return this.blog
             })
             .catch(err => {
                 console.log(err);
             })
+      },
+      movePost(param) {
+        let self = this;
+        self.getPost = param;
+        return self.getPost;
       }
-
   },
   mounted() {
-    this.fetchArticles()
+    this.fetchArticles();
+    // let { id } = this.$route.params
+    console.log(this.$route, 'ROUTE')
+    
+    // const response = await fetch(`/works/${id}`)
+    // console.log({id})
+  },
+  computed: {
+    singlePostView() {
+      let self = this;
+      let blogs = this.blog
+      if(this.getPost === null) { 
+        return false
+      } else {
+        for(let items in blogs) {
+          if(blogs[items].permalink === this.getPost) {
+            this.singlePost = blogs[items];
+          }
+        }
+      }
+      return this.singlePost;
+    }
   }
+  
 }
 </script>
 
-<style>
+<style lang="scss">
 .home-links a {
   margin-right: 1rem;
 }
 .blog-list {
-    padding: 2em 1em;
-    list-style: none;
+  table-layout: fixed;
+  width: 100%;
+  & tr {
+    border-bottom: 1px solid #000;
+    &:first-of-type {
+      border-top: 1px solid #000;
+      background-color: #000;
+      color: #fff;
+      th {
+        border-right-color: #fff;
+        &:last-of-type {
+          border-right-color: #000;
+        }
+      }
+    }
+  }
+  & td, & th {
+    border-right: 1px solid #000;
+    padding: .5em;
+    overflow: auto;
+    &:first-of-type {
+      border-left: 1px solid #000;
+    }
+  }
+  
 }
-.blog-list__item:first-of-type {
-    border-top: 1px solid #ccc;
-    padding: 1em 0;
+.local-link__item {
+  display: inline-block;
+  border: 1px solid #000;
+  padding: .2em;
 }
 </style>
