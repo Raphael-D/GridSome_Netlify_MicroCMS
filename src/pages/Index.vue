383b2f9debd3be37dbf88516d12aabf94016b068
Raphael-D <31120433+Raphael-D@users.noreply.github.com>
2019-12-02 13:04:54 +0900

    Changes to be committed: 	modified:   README.md 	modified:   package-lock.json 	modified:   package.json 	new file:   src/assets/images/._site-logo.png 	new file:   src/assets/images/site-logo.png 	modified:   src/assets/scss/_mixins.scss 	modified:   src/assets/scss/common.scss 	modified:   src/assets/scss/ress.scss 	modified:   src/layouts/Default.vue 	modified:   src/pages/Index.vue 	modified:   static/css/common.css 	modified:   static/css/ress.css

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,21 +1,28 @@
 <template>
   <Layout>
+    <div class="hero">
+        <ul class="hero__carousel">
+          <li class="hero__image" v-for="item in $page.allHome.edges[0].node.carousel" :key="item.id">
+            <img :src="item.carousel.url" />
+          </li>
+        </ul>
+        <h1 class="hero__heading">{{ $page.allHome.edges[0].node.title }}</h1>
+    </div>
     <section class="l-section">
       <div class="l-wrapper">
         <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-        <g-image alt="Example image" src="~/favicon.png" width="135" />
 
-        <h1>日本のフロントエンドのためのJAMStack開発中サイト by GridSome, Netlify, MicroCMS, Github</h1>
+        <!-- <h1>日本のフロントエンドのためのJAMStack開発中サイト by GridSome, Netlify, MicroCMS, Github</h1>
         <h1 class="ruby-text">Developing JAMStack by GridSome, Netlify, MicroCMS, Github for FrontEnd in Japan</h1>
         <a target="_blank" href="https://github.com/Raphael-D/GridSome_Netlify_MicroCMS">
           ソースはこちら<span class="ruby-text">Source here</span>
-        </a><small>※最も進行しているのは「feature」ブランチです。<span class="ruby-text">The most advanced is the “feature” branch.</span></small>
+        </a><small>※最も進行しているのは「feature」ブランチです。<span class="ruby-text">The most advanced is the “feature” branch.</span></small> -->
 
         <p class="home-links">
           <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
           <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
         </p>
-        <h1 style="background-color: #00f; color: #fff; padding: .5em; text-align: center;">{{ $page.allHome.edges[0].node.title }}</h1>
+        
         <ul>
           <li v-for="item in $page.allHome.edges[0].node.feature" :key="item.id">
             <h2>{{ item.feature_title }}</h2>
@@ -67,7 +74,13 @@ query {
 
 </page-query>
 <script>
+import {Carousel,Slide} from 'vue-carousel';
+
 export default {
+  components: {
+    Carousel,
+    Slide
+  },
   metaInfo: {
     title: 'microCMSをGridSomeで構築してNetlifyで公開するための検証サイト'
   },
