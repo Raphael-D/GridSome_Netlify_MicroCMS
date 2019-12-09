370d6d7146312ebed2225b09cde1e125568d8611
Raphael-D <31120433+Raphael-D@users.noreply.github.com>
2019-12-09 12:55:48 +0900

    Added: loading animation plugin

--- src/App.vue
+++ src/App.vue
@@ -35,3 +35,27 @@ export default {
   }
 }
 </script>
+<style lang="scss">
+#app .overlay-left[data-v-4c22b934],
+#app .overlay-right[data-v-4c22b934],
+#app .overlay-top[data-v-4c22b934],
+#app .overlay-bottom[data-v-4c22b934]   {
+    background: #fff !important;
+    z-index: 100;
+}
+:root {
+  --overlay-bg: #fff!important;
+}
+// .overlay-up-leave-active {
+//   background-color: #fff!important;
+// }
+// .overlay-up-leave-to {
+//   background-color: #fff!important;
+// }
+// .overlay-up-enter-active {
+//   background-color: #fff!important;
+// }
+// .overlay-up-enter-to {
+//   background-color: #fff!important;
+// }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,19 +1,46 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
+// Default.vue apply to global component.
 import DefaultLayout from '~/layouts/Default.vue'
+
+// Header apply to global component.
 import Header from '~/layouts/Header.vue'
+
+// Footer apply to global component.
 import Footer from '~/layouts/Footer.vue';
+
+// Vue font awesome
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+// Font awesome svg type
 import { config, library } from '@fortawesome/fontawesome-svg-core'
+
+// Font awesome solid type
 import { fas } from '@fortawesome/free-solid-svg-icons'
+
+// Font awesome brand type
 import { fab } from '@fortawesome/free-brands-svg-icons'
+
+// Font awesome regular type
 import { far } from '@fortawesome/free-regular-svg-icons';
+
+// Font awesome style sheet
 import '@fortawesome/fontawesome-svg-core/styles.css'
+
+// Handmade plugin Mobile window width check
 import mobileDetect from './mobiledetect';
+
 config.autoAddCss = false;
-library.add(fas, fab, far)
+
+// Font awesome add to library
+library.add(fas, fab, far);
+
+// Page transition plugin
 import VuePageTransition from 'vue-page-transition';
+
+// Loading animation component
+import VueLoading from 'vue-loading-template';
 
 
 
@@ -30,4 +57,5 @@ export default function (Vue, { router, head, isClient }) {
   Vue.component('Header', Header)
   Vue.component('Footer', Footer)
   Vue.component('font-awesome-icon', FontAwesomeIcon)
+  Vue.use(VueLoading)
 }
