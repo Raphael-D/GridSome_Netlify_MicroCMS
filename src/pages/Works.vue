6fcd9821131e78e9723e6945aab3d00544374974
Raphael-D <31120433+Raphael-D@users.noreply.github.com>
2019-12-10 14:03:38 +0900

    Update: css style

--- src/pages/Works.vue
+++ src/pages/Works.vue
@@ -1,35 +1,21 @@
 <template>
   <Layout>
+    <div class="hero">
+      <h1 class="hero__heading">投稿一覧ページ</h1>
+    </div>
+    <section class="l-section">
     <div class="l-wrapper">
-    
-    <nav class="local-link">
-      <a @click="movePost(item.permalink)" v-for="item in blog" :key="item.id" class="local-link__item" :href="'/works/' + item.permalink">{{ item.page_title }}</a>
-    </nav>
-
-      <div class="works-post">
-        <h1 class="works-post__title">Post list from GraphQL</h1>
-        <table class="works-post__table">
-          <tr class="works-post__list works-post__list--title">
-            <th>id</th>
-            <th>title</th>
-            <th>slug</th>
-            <th>date</th>
-            <th>content</th>
-            <th>link</th>
-          </tr>
-          <tr v-for="item in $page.allWorks.edges" :key="item.node.permalink" class="works-post__list">
-            <td>{{ item.node.id }}</td>
-            <td>{{ item.node.title }}</td>
-            <td>{{ item.node.slug }}</td>
-            <td>{{ item.node.date }}</td>
-            <td v-html="item.node.content"></td>
-            <td><g-link :to="item.node.path">{{ item.node.title }}</g-link></td>
-          </tr>
-        </table>
-      </div>
+      <article class="posts works-posts">
+          <section v-for="(item, key) in blog" :key="key" class="post-list">
+            <h2 class="post-list__title">{{ item.node.title }}</h2>
+            <time class="post-list__date"><font-awesome-icon :icon="['fas', 'clock']" />{{ item.node.date }}</time>
+            <div class="post-list__content">{{ rawcontent[key] }}</div>
+            <div class="post-list__link"><g-link :to="item.node.path">READ MORE<font-awesome-icon class="post-list__icon" :icon="['fas', 'angle-right']" /></g-link></div>
+          </section>
+      </article>
 
     </div>
-
+    </section>
   </Layout>
 </template>
 
@@ -54,132 +40,40 @@ query {
 <script>
 import axios from 'axios'
 export default {
-  metaInfo: {
-    title: 'Hello, world!'
-  },
   data() {
     return {
       blog: null,
-      singlePost: [],
-      getPost: null
+      rawcontent: []
     }
   },
+  metaInfo: {
+    title: 'GridSome & MicroCMS 投稿一覧ページのサンプル',
+    meta: [
+        { name: 'keywords', content: 'JAMStack, SSR, MicroCMS, GridSome, Netlify, Github, Vue, Static Site Generator, JAPAN'},
+        { name: 'description', content: 'このページは、MicroCMSとGridSomeのJAMStack開発環境で作成された投稿一覧ページです。静的サイトジェネレーターを利用しているのでページ表示が高速です。参考ソースはGithubよりご覧ください。'}
+    ],
+    link: [
+      { rel: 'stylesheet', href: '/css/style.css' }
+    ]
+  },
   methods: {
-    fetchArticles() {
-          axios
-            .get('https://codehack.microcms.io/api/v1/blog', {
-                headers: {'X-API-KEY': '6ccedd0a-a90d-45f3-801f-1a10abf108f4'}
-            })
-            .then(res => {
-                this.blog = res.data.contents;
-                console.log(this.blog[0].permalink, 'fjdajfka')
-                return this.blog
-            })
-            .catch(err => {
-                console.log(err);
-            })
-      },
-      movePost(param) {
-        let self = this;
-        self.getPost = param;
-        return self.getPost;
+      putData() {
+        this.blog = this.$page.allWorks.edges;
+        for(let item in this.blog) {
+          this.rawcontent.push(this.blog[item].node.content.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g,''));
+        }
       }
   },
   mounted() {
-    this.fetchArticles();
+    this.putData();
     
   },
   computed: {
-    singlePostView() {
-      let self = this;
-      let blogs = this.blog
-      if(this.getPost === null) { 
-        return false
-      } else {
-        for(let items in blogs) {
-          if(blogs[items].permalink === this.getPost) {
-            this.singlePost = blogs[items];
-          }
-        }
-      }
-      return this.singlePost;
-    }
   }
   
 }
 </script>
 
 <style lang="scss">
-.home-links a {
-  margin-right: 1rem;
-}
-.blog-list {
-  table-layout: fixed;
-  width: 100%;
-  & tr {
-    border-bottom: 1px solid #000;
-    &:first-of-type {
-      border-top: 1px solid #000;
-      background-color: #000;
-      color: #fff;
-      th {
-        border-right-color: #fff;
-        &:last-of-type {
-          border-right-color: #000;
-        }
-      }
-    }
-  }
-  & td, & th {
-    border-right: 1px solid #000;
-    padding: .5em;
-    overflow: auto;
-    &:first-of-type {
-      border-left: 1px solid #000;
-    }
-  }
-  
-}
-.local-link__item {
-  display: inline-block;
-  border: 1px solid #000;
-  padding: .2em;
-}
-// Works post
 
-.works-post {
-  padding-top: 100px;
-  &__title {
-    color: orange;
-    font-size: 3em;
-    text-align: center;
-    background-color: green;
-    margin-bottom: 1em;
-  }
-  &__table {
-    margin-bottom: 100px;
-    
-  }
-  &__list {
-    border-bottom: 1px solid gray;
-    &--title {
-      background-color: darkolivegreen;
-      color: darksalmon;
-
-    }
-    & td, & th {
-      padding: .5em;
-      border-left: 1px solid gray;
-      &:last-of-type {
-        border-right: 1px solid gray;
-      }
-    }
-    & th:not(:first-of-type){
-      border-left-color: #fff;
-    }
-    & th:not(:last-of-type) {
-      border-right-color: #fff;
-    }
-  }
-}
 </style>
